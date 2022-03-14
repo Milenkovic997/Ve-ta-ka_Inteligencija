a1c9aa9d39d01f2891fa84320603bf47b73e6d71
Stefan Milenković <milenkovic.stefan1@gmail.com>
2022-03-14 16:15:19 +0100

    Add project files.

---
+++ Veštačka_Inteligencija/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Veštačka_Inteligencija.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Instalacija()
+        {
+            return View();
+        }
+
+        public IActionResult OsnovePythona()
+        {
+            return View();
+        }
+        public IActionResult Vezba1()
+        {
+            return View();
+        }
+        public IActionResult Vezba2()
+        {
+            return View();
+        }
+        public IActionResult Vezba3()
+        {
+            return View();
+        }
+        public IActionResult Vezba4()
+        {
+            return View();
+        }
+        public IActionResult Vezba5()
+        {
+            return View();
+        }
+        public IActionResult Vezba6()
+        {
+            return View();
+        }
+        public IActionResult Vezba7()
+        {
+            return View();
+        }
+    }
+}
---
+++ Veštačka_Inteligencija/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Veštačka_Inteligencija.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
